3295016b2a9ce864388b1b094e93ea49222a8ff0
TestSheepNZ <mike_talks@hotmail.com>
2016-07-31 22:04:09 +1200

    Testing ability to commit

---
+++ HelloWorld-basic/src/HelloWorld.java
@@ -0,0 +1,13 @@
+// Okay this is just hello world
+// Right now I'm just using to trial uploading to Git
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+	    // Prints "Hello, World" in the terminal window.
+	    System.out.println("Hello, World");
+	    
+	    //Track this change
+	}
+
+}
